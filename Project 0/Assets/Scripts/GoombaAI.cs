b382c3a5b49ea263dcb7cdf2e94bfdb6128f3c52
David Liu <xdliuu95@hotmail.com>
2016-11-03 16:05:43 -0700

    Fixed Where Mario could jump
    Previously was unable to jump when he was standing on a block or pipe

--- Project 0/Assets/Scripts/GoombaAI.cs
+++ Project 0/Assets/Scripts/GoombaAI.cs
@@ -3,21 +3,58 @@ using System.Collections;
 
 public class GoombaAI : MonoBehaviour {
 	
-	public float speed;
-	Rigidbody2D rbGoomba;
+	public float speed = -1.5f;
+    public Transform sightStart;
+    public Transform sightEnd;
+    public bool collide;
+
+    public Transform weakness;
 
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
 	{
-		speed = -1.5f;
-		rbGoomba = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		Vector2 moveDir = new Vector2(speed, rbGoomba.velocity.y);
-	    rbGoomba.velocity = moveDir;
-	}
+        GetComponent<Rigidbody2D>().velocity = new Vector2(speed, GetComponent<Rigidbody2D>().velocity.y);
+
+        collide = Physics2D.Linecast(sightStart.position, sightEnd.position);
+
+        if (collide)
+        {
+
+            transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+            speed *= -1;
+
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+
+            float height = col.contacts[0].point.y - weakness.position.y;
+
+            if (height > 0)
+            {
+                Dies();
+                col.rigidbody.AddForce(new Vector2(0, 200)); //float value for how high you bounce off
+
+
+            }
+
+
+        }
+
+
+    }
+
+    void Dies()
+    {
+        Destroy(this.gameObject, 0.5f); //float value for when it dies
+    }
 }
